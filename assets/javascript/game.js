11fa6d41060a26380be7f1f5521125472273ee3b
Josh L <joshtlandi@gmail.com>
2018-08-15 13:28:51 -0700

    Initial functionality and pics

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// variables
+var counter = 0;
+
+var playerNumber = 0;
+
+var wins = 0;
+
+var losses = 0;
+
+var numberToReach = Math.floor(Math.random() * 100) + 1;
+
+
+
+$("#numToReach").text(numberToReach);
