6e83dedbf86c0f9ec5f9916e8c8c907f0bd96b9e
Josh L <joshtlandi@gmail.com>
2018-08-17 14:08:35 -0700

    Further html infrastructure and JS vars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 // variables
+// ==============================================================================
 var counter = 0;
 
 var playerNumber = 0;
@@ -12,6 +13,34 @@ var numberToReach = Math.floor(Math.random() * 100) + 20;
 var numberOptions = Math.floor(Math.random() * 12) + 1;
 
 
+
+var crystal = {
+    purple: {
+        name: "purple",
+        value: 0
+    },
+    blue: {
+        name: "blue",
+        value: 0
+    },
+    amber: {
+        name: "amber",
+        value: 0
+    },
+    clear: {
+        name: "clear",
+        value: 0
+    },
+}
+
+// ==============================================================================
+//functions
+// ==============================================================================
+
+
+// ==============================================================================
+//main process
+// ==============================================================================
 
 $("#numToReach").text(numberToReach);
 
