7b8b3e1ed86f34fbfb06447a3fb67dc4a1dbaea0
Josh L <joshtlandi@gmail.com>
2018-08-16 18:30:49 -0700

    further JS experimentation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,14 @@ var losses = 0;
 
 var numberToReach = Math.floor(Math.random() * 100) + 20;
 
+var numberOptions = Math.floor(Math.random() * 12) + 1;
+
 
 
 $("#numToReach").text(numberToReach);
+
+
+
+$(".crystalImage").on("click"), function() {
+
+}
