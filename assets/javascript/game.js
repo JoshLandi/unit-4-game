e80365c141275bcaff92003cc9d365e2199a10c2
Josh L <joshtlandi@gmail.com>
2018-08-20 10:14:38 -0700

    sorry, i thought i had pushed this already

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,13 @@
 // variables
 // ==============================================================================
-var counter = 0;
-
-var playerNumber = 0;
 
 var wins = 0;
 
 var losses = 0;
 
-var numberToReach = Math.floor(Math.random() * 100) + 20;
+var currentScore = 0;
 
-
+var numberToReach = 0;
 
 var crystal = {
     purple: {
@@ -32,22 +29,91 @@ var crystal = {
 };
 
     // Scores current/target
-    var currentScore = 0
-    var numberToReach = Math.floor(Math.random() * 100) + 20;
     
 // ==============================================================================
 //functions
 // ==============================================================================
+var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min +1)) + min;
+};
+
+var numberToReach = getRandom(19, 120);
+//Check for win
+var checkWin = function() {
+
+    if (currentScore === numberToReach) {
+        alert("You Win!");
+        wins++
+        $("#wins").html(wins);
+        startGame();
+    }
+    
+    else if (numberToReach < currentScore) {
+        alert("You Lost!");
+        losses++
+        $("#losses").html(losses);
+        startGame();
+    };
+};
+
+var startGame = function() {
+    // Resets
+    currentScore = 0;
+    numberToReach = getRandom(19, 120);
+
+    crystal.purple.value = getRandom(1, 12);
+    crystal.blue.value = getRandom(1, 12);
+    crystal.amber.value = getRandom(1, 12);
+    crystal.clear.value = getRandom(1, 12);
+    //Change Html
+
+    $("#scoreCount").html(currentScore);
+    $("#numToReach").html(numberToReach);
+
+
+    console.log("=========================");
+    console.log("Target Score: " + numberToReach);
+    console.log(currentScore);
+    console.log(crystal.purple.value + " " + crystal.blue.value + " " + crystal.amber.value + " " + crystal.clear.value);
+};
+
+// Changing Scores
+var addPoints = function (crystal) {
+
+    currentScore = currentScore + crystal.value;
+
+    $("#scoreCount").html(currentScore);
+
+    checkWin();
+
+    console.log("Score: " + currentScore);
+};
+
+// Reset game
 
 
 // ==============================================================================
 //main process
 // ==============================================================================
 
-$("#numToReach").text(numberToReach);
+
+startGame();
 
 
+$("#purple").click(function() {
+   addPoints(crystal.purple);
+});
 
-$(".crystalImage").on("click"), function() {
+$("#blue").click(function() {
+    addPoints(crystal.blue);
+});
 
-}
+$("#amber").click(function() {
+    addPoints(crystal.amber);
+});
+
+$("#clear").click(function() {
+    addPoints(crystal.clear);
+});
+
+
